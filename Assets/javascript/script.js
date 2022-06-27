1446489bb648a0c5f69c895ce52c47d8c8b96d6e
Ada Chika <adachika18@gmail.com>
2022-06-27 11:03:56 -0400

    Countdown timer function created

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -49,39 +49,27 @@ var questions = [
 
 ];
 
- start.addEventListener("click",function() {
-
- }
- )
- /*function setTime() {
-    // Sets interval in variable
-    //The setInterval() method takes a function as its first argument
+ // Function to set the countdown timer
+ function setTime() {
     var timerInterval = setInterval(function() {
-      //The function is the action that we want to be evaluated at each interval. This function prints a message at each interval
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+      timeLeft--;
+      timeEl.textContent = timeLeft + "s";
   
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        //When the countdown reaches zero, we want the action to stop. The clearInterval() method stops the execution of the interval. It takes one parameter: the variable name assigned to the interval
+      if(timeLeft === 0 || currentIndex === questions.length) {
         clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
+        timeEl.textContent = "";
+        alert("All done");
+        results();
       }
       //The second argument that the setInterval() method takes is the interval. Because intervals use milliseconds, we use 1000 to create an interval of one second
     }, 1000);
   }
   
-  // Function to create and append colorsplosion image
-  //We call sendMessage() after the interval is cleared, so the image will pop up after the countdown is complete. The sendMessage function creates and appends the image to the document
-  function sendMessage() {
-    timeEl.textContent = " ";
-    var imgEl = document.createElement("img");
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
+
     
   
-  }
   
-  setTime();*/
-  
+  start.addEventListener("click",function() {
+
+}
+)
